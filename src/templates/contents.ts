d8334621db566b36305e9a4495f7144a2978b3eb
Jamie Mill <jamiermill@gmail.com>
2026-02-07 23:54:24 +0100

    Add missing doctype to templates

--- src/templates/contents.ts
+++ src/templates/contents.ts
@@ -1,4 +1,5 @@
 const template = `doctype xml
+| <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 html(
   xmlns:epub='http://www.idpf.org/2007/ops',
   xmlns='http://www.w3.org/1999/xhtml'
--- src/templates/section.ts
+++ src/templates/section.ts
@@ -1,4 +1,5 @@
 const template = `doctype xml
+| <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 html(
   xml:lang=data.metadata.language,
   xmlns:epub='http://www.idpf.org/2007/ops',
